96a3e71aeee0812436a6626eb9d9de9d9bbda53d
yangzhou23 <yangzhou23@iflytek.com>
2025-11-21 10:40:39 +0800

    fix: fix install error

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../nextrec"))
 project = "NextRec"
 copyright = "2025, Yang Zhou"
 author = "Yang Zhou"
-release = "0.1.3"
+release = "0.1.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
